f499f5b43db0e9e8167be7c0141255d0d19d826c
1pondemo2njin3 <1pondemo2njin3@gmail.com>
2026-06-26 18:49:06 +0900

    Add files via upload

--- CSS_JS/slider.js
+++ CSS_JS/slider.js
@@ -1,12 +1,12 @@
 
 const swiper = new Swiper(".swiper", {
     breakpoints: {
-        1150: {slidesPerView: 5,},
-        975: {slidesPerView: 4,},
-        760: {slidesPerView: 3,},}, // 画像が重なる原因 *数字が合ってない場合あり
+        1220: {slidesPerView: 5,},
+        990: {slidesPerView: 4,},
+        640: {slidesPerView: 3,},}, // 画像が重なる原因 *数字が合ってない場合あり
 
     slidesPerView: 3,       // 同時に表示するスライド数 *設定値の2倍以上のスライドを用意
-    spaceBetween: 5,        // スライド間の余白
+    spaceBetween: 3,        // スライド間の余白
     speed: 5000,            // 移動速度(ms)
     loop: true,             // 無限ループ
     autoplay: {delay: 0,},  // 途切れない動きに必須
